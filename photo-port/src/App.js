e0d439d09f010b30d933b5a1d4ebeaf0d79060ae
unknown <jtjackson325@gmail.com>
2022-02-01 13:09:46 -0700

    got elements for the component form imported onto the app.js file

--- photo-port/src/App.js
+++ photo-port/src/App.js
@@ -2,6 +2,8 @@ import React, { useState } from 'react';
 import Nav from './components/Nav';
 import About from './components/About';
 import Gallery from './components/Gallery';
+import Contact from './components/Contact';
+import ContactForm from './components/Contact';
 
 function App() {
   const [categories] = useState([
@@ -25,8 +27,9 @@ function App() {
      ></Nav>
      <main>
        <div>
-       <Gallery currentCategory={currentCategory}></Gallery>
-       <About></About>
+        <ContactForm></ContactForm>
+        <Gallery currentCategory={currentCategory}></Gallery>
+        <About></About>
        </div>
      </main>
    </div>
