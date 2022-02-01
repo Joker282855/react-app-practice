d0b5593bc09648269d388897c56d6dae2e274a66
unknown <jtjackson325@gmail.com>
2022-02-01 13:56:22 -0700

    gave the html elements that caputre the inputs functinality

--- photo-port/src/components/Contact/index.js
+++ photo-port/src/components/Contact/index.js
@@ -1,21 +1,32 @@
-import React from 'react';
+import React, { useState } from 'react';
+
+const [formState, setFormState] = useState({ name: '', email: '', message: '' });
+
+const { name, email, message } = formState;
 
 function ContactForm() {
+    
+    function handleChange(e) {
+        setFormState({...formState, [e.target.name]: e.target.value })
+    }
+
+    console.log(formState);
+
     return (
         <section>
             <h1>Contact Me</h1>
             <form id="contact-form">
                 <div>
                     <label htmlFor="name">Name:</label>
-                    <input type="text" name="name" />
+                    <input type="text" name="name" defaultValue={name} onChange={handleChange} />
                 </div>
                 <div>
                     <label htmlFor="email">Email address:</label>
-                    <input type="text" name="email" />
+                    <input type="text" name="email" defaultValue={email} onChange={handleChange} />
                 </div>
                 <div>
                     <label htmlFor="message">Message:</label>
-                    <textarea name="message" rows="5" />
+                    <textarea name="message" rows="5" defaultValue={message} onChange={handleChange} />
                 </div>
                 <button type="submit">Submit</button>
             </form>
