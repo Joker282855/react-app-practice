251f55f82ae54a65fef1806003d44c4bb401f4b4
unknown <jtjackson325@gmail.com>
2022-02-19 21:02:18 -0700

    got the first two tests for the contact component to work

---
+++ photo-port/src/components/Contact/__tests__/index.test.js
@@ -0,0 +1,19 @@
+import React from 'react';
+import { render, cleanup } from '@testing-library/react';
+import '@testing-library/jest-dom/extend-expect';
+import Contact from '..';
+
+afterEach(cleanup); 
+
+describe('Contact form is rendering', () => {
+    
+    it('renders', () => {
+        render(<Contact />)
+    });
+
+    it('matches snapshot', () => {
+        const { asFragment } = render(<Contact />)
+        expect(asFragment()).toMatchSnapshot()
+    });
+});
+
