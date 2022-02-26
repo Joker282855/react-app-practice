a8f50ac2c797131a2fe5c043e0ad406316bcb6cc
unknown <jtjackson325@gmail.com>
2022-02-25 20:24:21 -0700

    coded the basic structure for testing the modal component

---
+++ photo-port/src/components/Modal/__tests__/index.test.js
@@ -0,0 +1,11 @@
+import React from 'react'
+import { render, cleanup, fireEvent } from '@testing-library/react'
+import '@testing-library/jest-dom/extend-expect'
+import Modal from '..'
+
+afterEach(cleanup)
+
+describe('Modal is rendering', () => {
+
+});
+
