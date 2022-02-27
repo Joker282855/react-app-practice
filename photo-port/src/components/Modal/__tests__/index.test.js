7cc10aa55edd40c5b1c8d3bcc750f892469804b6
unknown <jtjackson325@gmail.com>
2022-02-27 15:29:14 -0700

    got the first two tests for the Modal up and running correctly

--- photo-port/src/components/Modal/__tests__/index.test.js
+++ photo-port/src/components/Modal/__tests__/index.test.js
@@ -3,9 +3,34 @@ import { render, cleanup, fireEvent } from '@testing-library/react'
 import '@testing-library/jest-dom/extend-expect'
 import Modal from '..'
 
+const currentPhoto = { 
+    name: 'Park Bench', 
+    category: 'landscape', 
+    description: 'Lorem ipsum dolor sit amet, consectur adispicing elit. Nunc ultricide',
+    index: 1
+};
+const mocktoggleModal = jest.fn()
+
 afterEach(cleanup)
 
 describe('Modal is rendering', () => {
+    
+    it('renders', () => {
+        render(<Modal 
+            currentPhoto={currentPhoto}
+            toggleModal={mocktoggleModal}
+        />);
+    });
+
+    it('Matches snapshot', () => {
+        const { asFragment } = render(<Modal 
+            currentPhoto={currentPhoto}
+            toggleModal={mocktoggleModal}
+        />);
+
+        expect(asFragment()).toMatchSnapshot();
+
+    });
 
 });
 
